8f38757f79c731199eefc9a2629b702e70754740
Kevin Baltazar <markkevinbaltazar@gmail.com>
2016-12-18 16:12:47 +0800

    added new spritesheet for player

--- js/Preload.js
+++ js/Preload.js
@@ -6,7 +6,8 @@ TopDownGame.Preload.prototype = {
     preload: function() {
         this.load.tilemap('level1', 'assets/tilemap/first.json', null, Phaser.Tilemap.TILED_JSON);
         this.load.image('gameTiles', 'assets/map.png');
-        this.load.image('player', 'assets/star.png');
+        this.load.spritesheet('player', 'assets/dude.png', 32, 48);
+        this.load.image('star', 'assets/star.png');
     },
     create: function() {
         this.state.start('Game');
